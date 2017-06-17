d7fc8fbba204626b0ce2ea64320d98c58ccb8268
JackNGoodwin <jackgoodwinschmitz@gmail.com>
2017-06-17 11:02:40 +0100

    updates to p5 sketch - added tail & links

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,28 @@
 var pos, r;
+var seed;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
     pos = createVector(random(width), random(height));
     r = createVector(random(-1, 1), random(-1, 1));
+    seed = createVector(random(0, 1), random(0, 1));
 }
 
 function draw() {
-    background('#FEFEFA');
+    // background('#FEFEFA');
+    // if (frameCount % 5 == 0) {
+    noStroke();
+    fill(254, 254, 250, 1);
+    rect(-1, -1, width + 1, height + 1);
+    // }
 
     noFill();
     stroke(50);
     ellipse(pos.x, pos.y, 1, 1);
 
     pos.add((noise(r.x) * 2) - 1, (noise(r.y) * 2) - 1);
-    r.add(0.003, 0.0035);
+    r.add(0.08, 0.012);
 
     if (pos.x > width) pos.x = 0;
     if (pos.x < 0) pos.x = width;
