4385c591d29956f04ad05e8477c98181c942331b
JackNGoodwin <jackgoodwinschmitz@gmail.com>
2023-05-04 12:53:31 +0100

    updates to style and energy

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,28 @@
 var pos, r;
-var speed, seed;
+var vel, seed;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    speed = 10;
-    pos = createVector(random(width), random(height));
-    r = createVector(random(-speed, speed), random(-speed, speed));
+
+    pos = createVector(width/2, height/2);  
+    r = createVector(random(-1, 1), random(-1, 1));
     seed = createVector(random(0, 0.02), random(0, 0.02));
+    vel = createVector(random(1, 3), random(1, 3));
 }
 
 function draw() {
-    // background('#FEFEFA');
-    // if (frameCount % 5 == 0) {
-    noStroke();
-    fill(254, 254, 250, 1);
-    rect(-1, -1, width + 1, height + 1);
-    // }
+    fill(50);
+    stroke(150);
+    ellipse(pos.x, pos.y, 5, 5);
 
-    noFill();
-    stroke(50);
-    ellipse(pos.x, pos.y, 1, 1);
-
-    pos.add((noise(r.x) * 2) - 1, (noise(r.y) * 2) - 1);
+    pos.add((noise(r.x) * vel.x) - 1, (noise(r.y) * vel.y) - 1);
+  
     r.add(seed.x, seed.y);
 
     if (pos.x > width) pos.x = 0;
     if (pos.x < 0) pos.x = width;
     if (pos.y > height) pos.y = 0;
     if (pos.y < 0) pos.y = height;
-
-    textSize(12);
-    strokeWeight(0.7);
-    text(seed.x + ", " + seed.y, 50, height - 20);
 }
 
 function windowResized() {
